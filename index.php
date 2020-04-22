2af2e23659bfc229bfc060d9d1750f712f7824a0
Jasmin-Mediani <molihua1992@gmail.com>
2020-04-22 15:48:53 +0200

    milestone 1, metodo 2 (valori.php)

--- index.php
+++ index.php
@@ -16,5 +16,31 @@
     </div>
   </body>
 
-  <script src="main.js"></script>
+  <!-- <script src="main.js"></script> -->
+
+  <script>
+    $(document).ready(function() {
+        var mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre",
+            "Ottobre", "Novembre", "Dicembre"];
+
+        // var valori = [1000, 1322, 1123, 2301, 3288, 988, 502, 2300, 5332, 2300, 1233, 2322];
+
+        <?php include 'valori.php'; ?>
+        var valori = <?php echo json_encode($valori); ?>;
+
+        var ctx = $("#grafico1");
+        var chart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: mesi,
+                datasets: [{
+                    label: "Grafico numero 1",
+                    backgroundColor: "rgb(118, 215, 196)",
+                    borderColor: "rgb(52, 152, 219)",
+                    data: valori
+                }],
+            }
+        });
+    });
+  </script>
 </html>
